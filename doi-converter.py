e90b73176b1ecc57b287521a8a3326b51a830dee
James-Crean <creanjames@gmail.com>
2016-01-29 14:17:53 -0800

    basic functionality added, currently dumps json to file

--- doi-converter.py
+++ doi-converter.py
@@ -1,10 +1,10 @@
-#! /usr/bin/env python2
 import requests
 import json
 import sys
 import getopt
+import ipdb
 
-class RefClient(object):
+class DOIConverter(object):
     def __init__(self, accept='application/vnd.citationstyles.csl+json', timout=3):
         self.headers = {'accept': accept}
         self.timeout = timout
@@ -16,16 +16,9 @@ class RefClient(object):
     def query(self, doi):
         if doi.startswith("http://dx.doi.org/"):
             url = doi
-
         else:
             url = "http://dx.doi.org/" + doi
-
         r = requests.get(url, headers =self.headers)
-
-        if DEBUG:
-            print("Printing request output:")
-            print(r)
-            print("--------------------------------")
         return r
 
     def doi2json(self, doi):
@@ -35,27 +28,52 @@ class RefClient(object):
 def main(argv):
     inputfile = ""
     outputfile = ""
-    """
-    Begin checking command line options
-    """
+    converter = DOIConverter()
+    #
+    #Begin checking command line options
+    #
+    if len(argv) < 1:
+        print ("Usage: doi-converter.py -i <inputfile> -o <outputfile> -h")
+        sys.exit()
     try:
-        opts, args = getopt.getopt(argv, "hi:o:d",["help"])
+        opts, args = getopt.getopt(argv, "hi:o:",["help"])
     except getopt.GetoptError:
-        print ("Usage: doi-converter.py -i <inputfile> -o <outputfile> -h -d")
+        print ("Usage: doi-converter.py -h -d -i <inputfile> -o <outputfile>")
         print ("For help use -h and to enable debug messages use -d")
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ("Usage: doi-converter.py -i <inputfile> -o <outputfile> -h -d")
+            print ("Usage: doi-converter.py -h -d -i <inputfile> -o <outputfile>")
             print ("For help use -h and to enable debug messages use -d")
             sys.exit()
-        elif opt in ("-i"):
+        elif opt == "-i":
             inputfile = arg
-        elif opt in ("-o"):
+        elif opt == "-o":
             outputfile = arg
-    if inputfile == "" or outputfile ""
-        print("Error: Both input and output files must be specified."
+    if inputfile == "" or outputfile == "":
+        print("Error: Both input and output files must be specified.")
+        print("Inputfile=" + inputfile + " Outputfile=" + outputfile)
         sys.exit(2)
 
+    #
+    #End of command line checking
+    #Begin opening files and reading DOIs
+    #
+
+    inputfile = open(inputfile, 'r')
+    outputfile = open(outputfile, 'w')
+
+    #Warning: There should be a check for the output file being read only
+    #However, I do not currently know how to check that in python
+
+    for line in inputfile:
+        response = converter.doi2json(line)
+        json.dump(response, outputfile)
+        #outputfile.write(response)
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:]) #no need to include the script name in args
 
 
